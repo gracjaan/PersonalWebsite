4e63d113df6b8eec3f38a70dd5bc3e0dd8dd335e
gracjaan <gchmielnicki3005@gmail.com>
2023-09-15 20:14:33 +0200

    Adding colors to tailwind config

--- PersonalWebsite/tailwind.config.js
+++ PersonalWebsite/tailwind.config.js
@@ -5,7 +5,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#000000',
+        secondary: {
+          1: 'rgba(255, 255, 255)',
+          2: 'rgba(115, 21, 230)',
+        },
+        tertiary: '#F5F5F5',
+      },
+    },
   },
   plugins: [],
 }
