2ec3dfdb5083ccf393ac1dfa1fe736309ffe3423
gracjaan <gchmielnicki3005@gmail.com>
2023-09-15 20:14:56 +0200

    Removing redundancy

--- PersonalWebsite/src/App.jsx
+++ PersonalWebsite/src/App.jsx
@@ -1,11 +1,9 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Landing from './pages/Landing'
 
 function App() {
-  
+
   return (
     <Landing />
   )
