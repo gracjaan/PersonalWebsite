f693c38fce3e6e378a198bbfe7e04cd6e5b351db
gracjaan <gchmielnicki3005@gmail.com>
2023-09-16 13:45:30 +0200

    Passing props to modal navi

--- PersonalWebsite/src/components/Header.jsx
+++ PersonalWebsite/src/components/Header.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { Bars3Icon } from "@heroicons/react/24/solid";
+import NavigationModal from "../components/NavigationModal";
 
 const Header = () => {
+  const [openModal, setOpenModal] = useState(false);
+
   return (
     <header className="bg-black py-5 px-10 xl:py-10 xl:px-20 fixed top-0 left-0 right-0 flex justify-between items-center h-[92px] xl:h-[132px]">
       <div className="flex flex-col xl:flex-row xl:gap-5 justify-between xl:items-center">
@@ -25,8 +28,11 @@ const Header = () => {
         <button className="text-white text-lg leading-8 hover:text-primary transition ease-in hidden md:block border border-secondary-100 rounded-full px-8 py-4 hover:bg-secondary-100">
           Get in touch
         </button>
-        <Bars3Icon className="h-6 w-6 text-secondary-100 xl:hidden" />
+        <button className="h-6 w-6 text-secondary-100 xl:hidden" onClick={() => setOpenModal(true)}>
+          <Bars3Icon />
+        </button>
       </nav>
+      <NavigationModal openModal={openModal} setOpenModal={setOpenModal}/>
     </header>
   );
 };
