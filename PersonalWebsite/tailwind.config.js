6c21ea173c335e933027d85180f6a01a18319a0f
gracjaan <gchmielnicki3005@gmail.com>
2023-09-16 13:44:44 +0200

    Adding custom animations to slidedown and slideup

--- PersonalWebsite/tailwind.config.js
+++ PersonalWebsite/tailwind.config.js
@@ -15,6 +15,20 @@ export default {
         tertiary: 'rgba(162, 61, 224)',
         quaternary: 'rgba(115, 21, 230)',
       },
+      keyframes: {
+        slideUp: {
+          'from' : {transform: 'translateY(0)'},
+          'to' : {transform: 'translateY(-100%)'}
+        },
+        slideDown: {
+          'from' : {transform: 'translateY(-100%)'},
+          'to' : {transform: 'translateY(0)'}
+        }
+      },
+      animation: {
+        slideUp: 'slideUp 0.5s ease',
+        slideDown: 'slideDown 0.5s ease'
+      }
     },
   },
   plugins: [],
