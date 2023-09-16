a1310f4c0bffd1298a78543101c75a9c33d0ccdd
gracjaan <gchmielnicki3005@gmail.com>
2023-09-16 19:43:06 +0200

    Changing properties in animation

--- PersonalWebsite/tailwind.config.js
+++ PersonalWebsite/tailwind.config.js
@@ -27,10 +27,10 @@ export default {
         rotatingText: {
           "0%": { transform: "translateY(0)" },
           "28%": { transform: "translateY(0)" },
-          "33%": { transform: "translateY(-4.6rem)" },
-          "62%": { transform: "translateY(-4.6rem)" },
-          "67%": { transform: "translateY(-9.2rem)" },
-          "95%": { transform: "translateY(-9.2rem)" },
+          "33%": { transform: "translateY(-90px)" },
+          "62%": { transform: "translateY(-90px)" },
+          "67%": { transform: "translateY(-180px)" },
+          "95%": { transform: "translateY(-180px)" },
           "100%": { transform: "translateY(0)" },
         }
       },
