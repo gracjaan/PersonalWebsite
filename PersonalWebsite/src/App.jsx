84c2ea309bf1bc4ffa875e051cd9ec51b1dadef4
gracjaan <gchmielnicki3005@gmail.com>
2023-09-16 19:43:26 +0200

    Adding header and landing page to appjsx

--- PersonalWebsite/src/App.jsx
+++ PersonalWebsite/src/App.jsx
@@ -1,11 +1,18 @@
 import { useState } from 'react'
-import './App.css'
 import Landing from './pages/Landing'
+import Header from './components/Header'
 
 function App() {
 
   return (
-    <Landing />
+    <body className="bg-primary w-full">
+      <div className="max-w-[1920px] mx-auto">
+        <Header />
+        <main>
+          <Landing />
+        </main>
+      </div>
+    </body>
   )
 }
 
