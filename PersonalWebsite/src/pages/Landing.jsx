0224d33c59dacffdd413c1d8ae893c3ef340a3e0
gracjaan <gchmielnicki3005@gmail.com>
2023-09-16 17:42:38 +0200

    Landing page with rotating animation

--- PersonalWebsite/src/pages/Landing.jsx
+++ PersonalWebsite/src/pages/Landing.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
-import Header from '../components/Header'
-
+import React from "react";
 
 const Landing = () => {
   return (
-    <Header />
-  )
-}
+    <section className="px-40 h-[100vh]">
+      <h1 className="text-white text-7xl font-bold">Enhance your</h1>
+      <div className="h-16 overflow-hidden"> 
+        <h1 className="text-white text-7xl font-bold animate-rotatingText h-[4.6rem]">business development</h1>
+        <h1 className="text-white text-7xl font-bold animate-rotatingText h-[4.6rem]">application performance</h1>
+        <h1 className="text-white text-7xl font-bold animate-rotatingText h-[4.6rem]">developers' knowledge</h1>
+      </div>
+      <h1 className="text-white text-7xl font-bold">with me!</h1>
+    </section>
+  );
+};
 
-export default Landing
+export default Landing;
