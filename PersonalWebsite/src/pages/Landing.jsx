f3f73f5636c05df987ce6232bcc5429d0123c6cb
gracjaan <gchmielnicki3005@gmail.com>
2023-09-16 19:44:22 +0200

    Landing page reworked

--- PersonalWebsite/src/pages/Landing.jsx
+++ PersonalWebsite/src/pages/Landing.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const Landing = () => {
   return (
-    <section className="px-40 h-[100vh]">
-      <h1 className="text-white text-7xl font-bold">Enhance your</h1>
-      <div className="h-16 overflow-hidden"> 
-        <h1 className="text-white text-7xl font-bold animate-rotatingText h-[4.6rem]">business development</h1>
-        <h1 className="text-white text-7xl font-bold animate-rotatingText h-[4.6rem]">application performance</h1>
-        <h1 className="text-white text-7xl font-bold animate-rotatingText h-[4.6rem]">developers' knowledge</h1>
+    <section className="px-10 lg:px-20 2xl:px-40 pt-40 h-screen">
+      <h1 className="text-white text-7xl font-bold leading-[90px]">Enhance your</h1>
+      <div className="h-[90px] overflow-hidden"> 
+        <h1 className="text-white text-7xl font-bold animate-rotatingText leading-[90px]">business development</h1>
+        <h1 className="text-white text-7xl font-bold animate-rotatingText leading-[90px]">application performance</h1>
+        <h1 className="text-white text-7xl font-bold animate-rotatingText leading-[90px]">developers' knowledge</h1>
       </div>
-      <h1 className="text-white text-7xl font-bold">with me!</h1>
+      <h1 className="text-white text-7xl font-bold leading-[90px]">with me!</h1>
     </section>
   );
 };
