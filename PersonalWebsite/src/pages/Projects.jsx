155211f93aa08f1c7e086cc3a6d7d87b1e7db396
gracjaan <gchmielnicki3005@gmail.com>
2023-09-17 00:23:18 +0200

    Finished projects page (without data)

--- PersonalWebsite/src/pages/Projects.jsx
+++ PersonalWebsite/src/pages/Projects.jsx
@@ -1,9 +1,68 @@
-import React from 'react'
+import React from "react";
+import Logo from "../assets/projects.webp";
+import { ArrowRightIcon } from "@heroicons/react/24/solid";
 
 const Projects = () => {
   return (
-    <div>Projects</div>
-  )
-}
+    <section className="mt-[320px] px-10 lg:px-20 2xl:px-40 bg-primary">
+      <h2 className="text-white text-7xl font-normal leading-[70px] mb-10">
+        Projects
+      </h2>
+      <div className="flex flex-col 2xl:flex-row">
+        <div className="flex flex-col flex-1 2xl:max-w-[45%]">
+          <h4 className="text-white text-3xl font-normal leading-[70px]">
+            I over 10 years of commercial experience and 30+ successful business
+            cooperations.
+          </h4>
+          <ul className="mt-5">
+            <li className="text-white text-3xl font-normal leading-[70px] mb-5 border-b">
+              <button className="w-full p-[10px] flex flex-row justify-between items-center">
+                <h4 className="text-white text-3xl font-normal leading-[70px]">
+                  styll.
+                </h4>
+                <ArrowRightIcon className="text-white h-[30px]" />
+              </button>
+            </li>
+            <li className="text-white text-3xl font-normal leading-[70px] mb-5 border-b">
+              <button className="w-full p-[10px] flex flex-row justify-between items-center">
+                <h4 className="text-white text-3xl font-normal leading-[70px]">
+                  styll.
+                </h4>
+                <ArrowRightIcon className="text-white h-[30px]" />
+              </button>
+            </li>
+            <li className="text-white text-3xl font-normal leading-[70px] mb-5 border-b">
+              <button className="w-full p-[10px] flex flex-row justify-between items-center">
+                <h4 className="text-white text-3xl font-normal leading-[70px]">
+                  styll.
+                </h4>
+                <ArrowRightIcon className="text-white h-[30px]" />
+              </button>
+            </li>
+            <li className="text-white text-3xl font-normal leading-[70px] mb-5 border-b">
+              <button className="w-full p-[10px] flex flex-row justify-between items-center">
+                <h4 className="text-white text-3xl font-normal leading-[70px]">
+                  styll.
+                </h4>
+                <ArrowRightIcon className="text-white h-[30px]" />
+              </button>
+            </li>
+            <li className="text-white text-3xl font-normal leading-[70px] mb-5 border-b">
+              <button className="w-full p-[10px] flex flex-row justify-between items-center">
+                <h4 className="text-white text-3xl font-normal leading-[70px]">
+                  styll.
+                </h4>
+                <ArrowRightIcon className="text-white h-[30px]" />
+              </button>
+            </li>
+          </ul>
+        </div>
+        <div className="flex flex-1 justify-center items-center">
+          <img src={Logo} alt="Logo" className="h-[660px]" />
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Projects
+export default Projects;
