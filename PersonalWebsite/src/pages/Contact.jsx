7d3c8e4b2cb3ac3ecf37171af68bfee1dc53d56d
gracjaan <gchmielnicki3005@gmail.com>
2023-09-17 01:15:25 +0200

    Finished contact form

--- PersonalWebsite/src/pages/Contact.jsx
+++ PersonalWebsite/src/pages/Contact.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
+import Logo from "../assets/contact.webp";
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <section className="mt-[320px] px-10 lg:px-20 2xl:px-40 bg-primary flex flex-col 2xl:flex-row">
+      <div className="flex flex-col flex-1 2xl:max-w-[45%]">
+        <h2 className="text-white text-7xl font-normal">Contact</h2>
+        <form className="flex flex-col gap-5 my-10">
+          <input
+            placeholder="name"
+            name="name"
+            className="border border-white/[.24] rounded-lg bg-transparent p-4 text-white text-lg font-normal"
+          ></input>
+          <input
+            placeholder="email"
+            name="email"
+            className="border border-white/[.24] rounded-lg bg-transparent p-4 text-white text-lg font-normal"
+          ></input>
+          <textarea
+            placeholder="message"
+            name="message"
+            rows={2}
+            className="border border-white/[.24] rounded-lg bg-transparent p-4 text-white text-lg font-normal"
+          ></textarea>
+          <button className="text-white text-lg leading-8 hover:text-primary transition ease-in hidden md:block border border-secondary-100 rounded-full px-8 py-4 hover:bg-secondary-100 self-start mt-5">
+            Send me a message!
+          </button>
+        </form>
+      </div>
+      <div className="flex flex-1 justify-center items-center">
+        <img src={Logo} alt="Logo" className="h-[496px]" />
+      </div>
+    </section>
+  );
+};
 
-export default Contact
+export default Contact;
