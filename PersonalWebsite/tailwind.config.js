299eda9decc2e5e7ab830554fa52fd7f6a6af57d
gracjaan <gchmielnicki3005@gmail.com>
2023-09-17 22:37:29 +0200

    Adding 4 new animations to handle main modal

--- PersonalWebsite/tailwind.config.js
+++ PersonalWebsite/tailwind.config.js
@@ -21,6 +21,14 @@ export default {
           'from' : {transform: 'translateY(-100%)'},
           'to' : {transform: 'translateY(0)'}
         },
+        slideUp2: {
+          'from' : {transform: 'translateY(100%)'},
+          'to' : {transform: 'translateY(0)'}
+        },
+        slideDown2: {
+          'from' : {transform: 'translateY(0)'},
+          'to' : {transform: 'translateY(100%)'}
+        },
         rotatingText1: {
           "0%": { transform: "translateY(0)" },
           "28%": { transform: "translateY(0)" },
@@ -48,13 +56,35 @@ export default {
           "95%": { transform: "translateY(-240px)" },
           "100%": { transform: "translateY(0)" },
         },
+        fadeIn: {
+          'from': { opacity: 0 },
+          'to': { opacity: 1 },
+        },
+        fadeOut: {
+          'from': { opacity: 1 },
+          'to': { opacity: 0 },
+        },
+        slideLeft2: {
+          'from': { transform: 'translateX(100%)' },
+          'to': { transform: 'translateX(0)' },
+        },
+        slideRight2: {
+          'from': { transform: 'translateX(0)' },
+          'to': { transform: 'translateX(100%)' },
+        },
       },
       animation: {
         slideUp: 'slideUp 0.5s ease',
         slideDown: 'slideDown 0.5s ease',
+        slideUp2: 'slideUp2 0.5s ease',
+        slideDown2: 'slideDown2 0.5s ease',
         rotatingText1: 'rotatingText1 6s linear infinite',
         rotatingText2: 'rotatingText2 6s linear infinite',
         rotatingText3: 'rotatingText3 6s linear infinite',
+        fadeIn: 'fadeIn 0.5s ease',
+        fadeOut: 'fadeOut 0.5s ease',
+        slideLeft2: 'slideLeft2 0.5s ease',
+        slideRight2: 'slideRight2 0.5s ease',
       },
       fontFamily: {
         'ar': ["Aeonik-Regular", "Montserrat","Helvetica", "system-ui", "sans-serif"],
