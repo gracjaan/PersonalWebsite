d33faa8ca97d322001419667c99c43acfdac4835
gracjaan <gchmielnicki3005@gmail.com>
2023-09-17 11:37:50 +0200

    Extracting styles and oval button

--- PersonalWebsite/src/components/Header.jsx
+++ PersonalWebsite/src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Bars3Icon } from "@heroicons/react/24/solid";
 import NavigationModal from "../components/NavigationModal";
+import OvalButton from "./OvalButton";
 
 const Header = () => {
   const [openModal, setOpenModal] = useState(false);
@@ -8,31 +9,36 @@ const Header = () => {
   return (
     <header className="bg-black py-5 px-10 xl:py-10 xl:px-20 fixed top-0 left-0 right-0 flex justify-between items-center h-[92px] xl:h-[132px] max-w-[1920px] mx-auto z-50">
       <div className="flex flex-col xl:flex-row xl:gap-5 justify-between xl:items-center">
-        <div className="text-white text-lg font-normal leading-8">
-          Gracjan Chmielnicki
-        </div>
-        <div className="text-white text-sm xl:text-lg font-normal leading-8 text-secondary/[.64]">
+        <span>Gracjan Chmielnicki</span>
+        <span className="text-sm xl:text-lg text-secondary/[.6]">
           Full Stack Developer
-        </div>
+        </span>
       </div>
       <nav className="flex justify-between items-center gap-8">
-        <button className="text-white text-lg leading-8 hover:text-tertiary transition ease-in hidden xl:block">
-          Experience
+        <button>
+          <span className="hover:text-tertiary transition ease-in hidden xl:block">
+            Experience
+          </span>
         </button>
-        <button className="text-white text-lg leading-8 hover:text-tertiary transition ease-in hidden xl:block">
-          Education
+        <button>
+          <span className="hover:text-tertiary transition ease-in hidden xl:block">
+            Education
+          </span>
         </button>
-        <button className="text-white text-lg leading-8 hover:text-tertiary transition ease-in hidden xl:block">
-          Projects
+        <button>
+          <span className="hover:text-tertiary transition ease-in hidden xl:block">
+            Projects
+          </span>
         </button>
-        <button className="text-white text-lg leading-8 hover:text-primary transition ease-in hidden md:block border border-secondary rounded-full px-8 py-4 hover:bg-secondary">
-          Get in touch
-        </button>
-        <button className="h-6 w-6 text-secondary xl:hidden" onClick={() => setOpenModal(true)}>
+        <OvalButton text="Get in touch" />
+        <button
+          className="h-6 w-6 text-secondary xl:hidden"
+          onClick={() => setOpenModal(true)}
+        >
           <Bars3Icon />
         </button>
       </nav>
-      <NavigationModal openModal={openModal} setOpenModal={setOpenModal}/>
+      <NavigationModal openModal={openModal} setOpenModal={setOpenModal} />
     </header>
   );
 };
