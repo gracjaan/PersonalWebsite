7452aba36442fbd94375022f33f97f457a31c860
gracjaan <gchmielnicki3005@gmail.com>
2023-09-17 13:00:40 +0200

    Changing h4 to generic span styling

--- PersonalWebsite/src/components/Footer.jsx
+++ PersonalWebsite/src/components/Footer.jsx
@@ -1,22 +1,27 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
     <footer className="px-10 lg:px-20 2xl:px-40 bg-primary">
-        <div className="flex flex-row my-10 gap-10">
-            <button>
-                <h4 className="text-white text-lg font-normal">Linkedin</h4>
-            </button>
-            <button>
-                <h4 className="text-white text-lg font-normal">Twitter</h4>
-            </button>
-            <button>
-                <h4 className="text-white text-lg font-normal">GitHub</h4>
-            </button>
-        </div>
-        <div className="text-white text-lg font-normal py-5">Copyright © 2023 Michał Chudziak. All rights reserved. hello@michalchudziak.dev.</div>
+      <div className="flex flex-row my-10 gap-10">
+        <button>
+          <span>Linkedin</span>
+        </button>
+        <button>
+          <span>Twitter</span>
+        </button>
+        <button>
+          <span>GitHub</span>
+        </button>
+      </div>
+      <div className="py-5">
+        <span>
+          Copyright © 2023 Michał Chudziak. All rights reserved.
+          hello@michalchudziak.dev.
+        </span>
+      </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
