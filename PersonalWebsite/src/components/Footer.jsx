ab9f15f30c7788a87b7396b8deea2270b3815dd9
gracjaan <gchmielnicki3005@gmail.com>
2023-09-20 18:34:42 +0200

    Changing footer bg to transparent and transitionin

--- PersonalWebsite/src/components/Footer.jsx
+++ PersonalWebsite/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="px-10 lg:px-20 2xl:px-40 bg-primary">
+    <footer className="px-10 lg:px-20 2xl:px-40 transition-all ease-in duration-300">
       <div className="flex flex-row my-10 gap-10">
         <button>
           <span>Linkedin</span>
