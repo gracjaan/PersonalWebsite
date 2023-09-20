a973861f12c7307254c635f6734dd7a39ba7ca46
gracjaan <gchmielnicki3005@gmail.com>
2023-09-20 18:33:43 +0200

    Creating appcontext for an app

---
+++ PersonalWebsite/src/context/AppContext.jsx
@@ -0,0 +1,17 @@
+import React, { createContext, useContext, useState } from "react";
+
+const AppContext = createContext();
+
+export function useAppContext() {
+  return useContext(AppContext);
+}
+
+export function AppProvider({ children }) {
+  const [isIntroductionInMiddle, setIsIntroductionInMiddle] = useState(false);
+
+  return (
+    <AppContext.Provider value={{ isIntroductionInMiddle, setIsIntroductionInMiddle }}>
+      {children}
+    </AppContext.Provider>
+  );
+}
