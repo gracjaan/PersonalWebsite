91493902387476ce86ce754fd5b06a0c8b3d0b64
gracjaan <gchmielnicki3005@gmail.com>
2023-09-20 23:02:36 +0200

    Adding dynamic background color changes

--- PersonalWebsite/src/App.jsx
+++ PersonalWebsite/src/App.jsx
@@ -1,15 +1,40 @@
-import { useState } from 'react'
-import Landing from './pages/Landing'
-import Header from './components/Header'
-import Introduction from './pages/Introduction'
-import Projects from './pages/Projects'
-import Footer from './components/Footer'
-import Contact from './pages/Contact'
+import React, { useState, useEffect } from "react";
+import Landing from "./pages/Landing";
+import Header from "./components/Header";
+import Introduction from "./pages/Introduction";
+import Projects from "./pages/Projects";
+import Footer from "./components/Footer";
+import Contact from "./pages/Contact";
+import { useAppContext } from "./context/AppContext";
 
 function App() {
+  const { isBackgroundBlack, setisBackgroundBlack } = useAppContext();
+
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        console.log(entry);
+        if (entry.isIntersecting) {
+          setisBackgroundBlack(false);
+        }
+        else {
+          setisBackgroundBlack(true);
+        }
+      });
+    },
+    {
+      threshold: 0, rootMargin: "-50% 0px -50% 0px"
+    }
+  );
+
+  useEffect(() => {
+    // const sections = document.querySelectorAll("section");
+    // sections.forEach((section) => observer.observe(section));
+    observer.observe(document.getElementById("projects"))
+  }, []);
 
   return (
-    <div className="bg-primary w-full">
+    <div className={`w-full ${isBackgroundBlack ? "bg-primary" : "bg-quaternary"} transition ease-in duration-300`}>
       <div className="max-w-[1920px] mx-auto">
         <Header />
         <main>
@@ -21,7 +46,7 @@ function App() {
         </main>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
