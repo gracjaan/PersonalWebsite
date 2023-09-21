77ff10a64a588cf1f7241e34d0d141ecac992302
gracjaan <gchmielnicki3005@gmail.com>
2023-09-21 14:52:36 +0200

    Reduced margin and changed text

--- PersonalWebsite/src/pages/Introduction.jsx
+++ PersonalWebsite/src/pages/Introduction.jsx
@@ -4,21 +4,21 @@ import Grako from "../assets/grako.png";
 
 const Introduction = () => {
   return (
-    <section className="mt-[480px] px-10 lg:px-20 2xl:px-40 bg-transparent flex flex-col-reverse 2xl:flex-row" id="introduction">
+    <section className="mt-[320px] px-10 lg:px-20 2xl:px-40 bg-transparent flex flex-col-reverse 2xl:flex-row" id="introduction">
       <div className="flex flex-1 justify-center items-center">
         <img src={Grako} alt="Logo" className="h-[660px]" />
       </div>
       <div className="flex flex-col flex-1 gap-12">
         <h2 className="text-white text-7xl font-normal leading-[70px]">
-          Hi. I'm Gracjan Chmielnicki, and I am an independent full stack
-          developer with a track record of success in a variety of technologies.
+          Hi. I'm Gracjan Chmielnicki, and I am full stack
+          developer with a track record of success.
         </h2>
         <h2 className="text-tertiary text-7xl font-normal leading-[70px]">
-          Committed to delivering high-quality results and continuously striving
-          for personal and professional growth.
+          When I'm not coding, I grow businesses, play basketball, and enjoy life.
         </h2>
         <h2 className="text-white text-7xl font-normal leading-[70px]">
-          When I'm not coding, I grow businesses, play basketball, and enjoy life.
+          Committed to delivering high-quality results and continuously striving
+          for personal growth.
         </h2>
       </div>
     </section>
