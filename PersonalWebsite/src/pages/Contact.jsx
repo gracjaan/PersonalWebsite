8717e09e284a4f4f78501e7b7d7dd9da43df55d6
gracjaan <gchmielnicki3005@gmail.com>
2023-09-21 20:22:21 +0200

    Added input validation to contact form

--- PersonalWebsite/src/pages/Contact.jsx
+++ PersonalWebsite/src/pages/Contact.jsx
@@ -3,24 +3,49 @@ import Logo from "../assets/contact.webp";
 import OvalButton from "../components/OvalButton";
 
 const Contact = () => {
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    const name = document.getElementById("name");
+    const email = document.getElementById("email");
+    const message = document.getElementById("message");
+
+    if (name.value === "") {
+      document.getElementById("nameerror").classList.remove("hidden");
+    }
+    if (email.value === "") {
+      document.getElementById("emailerror").classList.remove("hidden");
+    }
+    if (message.value === "") {
+      document.getElementById("messageerror").classList.remove("hidden");
+    }
+  };
+
   return (
-    <section className="mt-[320px] px-10 lg:px-20 2xl:px-40 flex flex-col xl:flex-row" id="contact">
+    <section
+      className="mt-[320px] px-10 lg:px-20 2xl:px-40 flex flex-col xl:flex-row"
+      id="contact"
+    >
       <div className="flex flex-col flex-1 2xl:max-w-[45%]">
         <h2 className="text-white text-7xl font-normal">Contact</h2>
-        <form className="flex flex-col gap-5 my-10">
-          <input
-            placeholder="name"
-            name="name"
-          ></input>
-          <input
-            placeholder="email"
-            name="email"
-          ></input>
+        <form className="flex flex-col gap-5 my-10" onSubmit={sendEmail}>
+          <input placeholder="name" name="name" id="name"></input>
+          <span className="text-secondary/[0.4] hidden" id="nameerror">
+            This field is required.
+          </span>
+          <input placeholder="email" name="email" id="email"></input>
+          <span className="text-secondary/[0.4] hidden" id="emailerror">
+            This field is required.
+          </span>
           <textarea
             placeholder="message"
             name="message"
+            id="message"
             rows={2}
           ></textarea>
+          <span className="text-secondary/[0.4] hidden" id="messageerror">
+            This field is required.
+          </span>
           <button className="text-white text-lg leading-8 hover:text-primary transition ease-in hidden md:block border border-secondary rounded-full px-8 py-4 hover:bg-secondary self-start mt-5">
             Send me a message!
           </button>
