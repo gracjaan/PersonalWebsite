93abaf76e23a60035ca8fe379e076fd535ae5a80
gracjaan <gchmielnicki3005@gmail.com>
2023-09-23 18:54:23 +0200

    Adding 3d models to landing page

--- PersonalWebsite/src/pages/Landing.jsx
+++ PersonalWebsite/src/pages/Landing.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { ArrowDownIcon } from "@heroicons/react/24/solid";
 import OvalButton from "../components/OvalButton";
+import EarthCanvas from "../canvas/Earth";
+import StarsCanvas from "../canvas/Stars";
 
 const Landing = () => {
   return (
-    <section className="pt-40 h-screen min-h-[100vh] flex flex-col justify-between" id="landing">
-      <div className="px-5 sm:px-10 lg:px-20 2xl:px-40 transition-all ease-in duration-300">
+    <section className="pt-40 h-screen min-h-[100vh] flex flex-col justify-between relative" id="landing">
+      <div className="px-5 sm:px-10 lg:px-20 2xl:px-40 transition-all ease-in duration-300 z-10">
         <div className="pb-16">
           <h1>
             Enhance your
@@ -25,12 +27,18 @@ const Landing = () => {
             with me!
           </h1>
         </div>
-        <a className="w-fit text-white text-lg leading-8 hover:text-primary transition ease-in hidden md:block border border-secondary rounded-full px-8 py-4 hover:bg-secondary" href="/resume.pdf" download={true}>
+        <a className="w-fit text-white text-lg leading-8 hover:text-primary transition ease-in border border-secondary rounded-full px-8 py-4 hover:bg-secondary" href="/resume.pdf" download={true}>
           Download resume
         </a>
       </div>
       <div className="hidden 2xl:block px-40 mt-10 mb-[120px]">
         <ArrowDownIcon className="h-24 text-tertiary animate-bounce" />
+      </div>
+      <div className="absolute top-[500px] md:top-[400px] lg:top-[300px] xl:top-[200px] 2xl:top-[300px] right-[-100px] md:right-0 z-0 xl:z-20">
+        <div className="relative h-[700px] 2xl:h-[1000px] w-[700px] 2xl:w-[1000px] overflow-hidden">
+          <EarthCanvas />
+          <StarsCanvas />
+        </div>
       </div>
     </section>
   );
