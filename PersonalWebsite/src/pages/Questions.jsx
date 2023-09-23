af0dbd092951dccb60608fef4b74aa072c2aa6b9
gracjaan <gchmielnicki3005@gmail.com>
2023-09-23 19:40:23 +0200

    Added questions with extendable answers

--- PersonalWebsite/src/pages/Questions.jsx
+++ PersonalWebsite/src/pages/Questions.jsx
@@ -5,6 +5,15 @@ import ModalExperience from "../components/ModalExperience";
 import data from "../data/experience/data.json";
 
 const Questions = () => {
+
+  const handleButton = (id) => {
+    const answer = document.getElementById(id);
+    const listelement = document.getElementById(id + "0");
+    answer.classList.toggle("show");
+    listelement.classList.toggle("border-white/[.24]");
+    listelement.classList.toggle("border-white");
+  }
+
   return (
     <section className="mt-[240px] 2xl:mt-[320px] px-5 sm:px-10 lg:px-20 2xl:px-40 relative transition-all ease-in" id="questions">
       <div className="flex flex-col 2xl:flex-row">
@@ -13,36 +22,36 @@ const Questions = () => {
         </div>
         <div className="flex flex-col flex-1 2xl:max-w-[45%] z-20">
           <ul className="mt-10 2xl:mt-0 flex flex-col justify-center">
-            <li className="flex flex-col cursor-pointer border-t border-white/[.24] py-10">
-                <button className="text-left cursor-pointer pt-[2px] pb-[3px] px-[6px]">
+            <li className="flex flex-col cursor-pointer border-t border-white/[.24] pt-10 pb-5 transition-all ease-in duration-300" id="a00">
+                <button className="text-left cursor-pointer pt-[2px] pb-[3px] px-[6px]" onClick={() => handleButton("a0")}>
                     <div className="flex flex-row justify-between items-start">
                         <h5 className="text-secondary">What's your thing? Building apps, building businesses, or maybe something else?</h5>
                         <PlusIcon className="text-white h-[30px] ml-5" />
                     </div>
-                    <div className="hidden">
-                        <p>It is the mix of both. I incredibly enjoy software development since early days of high school. On the other hand, I always had entrepreneurial mind that always wants to exceed in this area.</p>
+                    <div className="answer" id="a0">
+                        <span className="mt-5">It is the mix of both. I incredibly enjoy software development since early days of high school. On the other hand, I always had entrepreneurial mind that always wants to exceed in this area.</span>
                     </div>
                 </button>
             </li>
-            <li className="flex flex-col cursor-pointer border-t border-white/[.24] py-10">
-                <button className="text-left cursor-pointer pt-[2px] pb-[3px] px-[6px]">
+            <li className="flex flex-col cursor-pointer border-t border-white/[.24] pt-10 pb-5 transition-all ease-in duration-300" id="a10">
+                <button className="text-left cursor-pointer pt-[2px] pb-[3px] px-[6px]" onClick={() => handleButton("a1")}>
                     <div className="flex flex-row justify-between items-start">
                         <h5 className="text-secondary">Where are you based and what's your availability during the day?</h5>
                         <PlusIcon className="text-white h-[30px] ml-5" />
                     </div>
-                    <div className="hidden">
-                        <p>I am based in Enschede, NL (CET time zone). However, I often visit Warsaw, PL. Moreover, I am more than willing to relocate. When it comes to availability, I can work between 20 and 40 hours a week.</p>
+                    <div className="answer" id="a1">
+                        <span className="mt-5">I am based in Enschede, NL (CET time zone). However, I often visit Warsaw, PL. Moreover, I am more than willing to relocate. When it comes to availability, I can work between 20 and 40 hours a week.</span>
                     </div>
                 </button>
             </li>
-            <li className="flex flex-col cursor-pointer border-t border-white/[.24] py-10">
-                <button className="text-left cursor-pointer pt-[2px] pb-[3px] px-[6px]">
+            <li className="flex flex-col cursor-pointer border-t border-white/[.24] pt-10 pb-5 transition-all ease-in duration-300" id="a20">
+                <button className="text-left cursor-pointer pt-[2px] pb-[3px] px-[6px]" onClick={() => handleButton("a2")}>
                     <div className="flex flex-row justify-between items-start">
                         <h5 className="text-secondary">What technologies do you work with?</h5>
                         <PlusIcon className="text-white h-[30px] ml-5" />
                     </div>
-                    <div className="hidden">
-                        <p>I use variety of technologies for different purposes. I'm definitely profficient in HTML/CSS/JS and React/ReactNative. I also have experience in building back-end systems in JAVA. When it comes to business software, I have certified profficiency in MS Office and Tableau. </p>
+                    <div className="answer" id="a2">
+                        <span className="mt-5">I use variety of technologies for different purposes. I'm definitely profficient in HTML/CSS/JS and React/ReactNative. I also have experience in building back-end systems in JAVA. When it comes to business software, I have certified profficiency in MS Office and Tableau. </span>
                     </div>
                 </button>
             </li>
