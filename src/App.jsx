471a9aa50da9a1eeeced9173b3674373a9d2a9be
Gracjan Chmielnicki <gchmielnicki3005@gmail.com>
2025-03-03 14:28:16 +0100

    Adding Icon website

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Nina from './pages/Nina';
 import Main from "./pages/Main";
 import BlankStudio from "./pages/temp/BlankStudio";
 import StanStore from "./pages/temp/StanStore";
+import Icon from "./pages/temp/Icon";
 import "./App.css";
 
 function App() {
   return (
-    <div>
-      <HashRouter>
-        <Routes>
-          <Route path="/stanstore" element={<StanStore />} />
-          <Route path="/blankstudio" element={<BlankStudio />} />
-          <Route path="/" element={<Main />} />
-        </Routes>
-      </HashRouter>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/stanstore" element={<StanStore />} />
+        <Route path="/blankstudio" element={<BlankStudio />} />
+        <Route path="/icon" element={<Icon />} />
+        <Route path="/" element={<Main />} />
+      </Routes>
+    </Router>
   );
 }
 
